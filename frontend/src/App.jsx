8aeecb7d75dcf6c681d4183b189363fdac5d89cf
laura24pena <lpenabar@fiu.edu>
2026-03-10 16:42:12 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
 // src/App.jsx
-import React from "react";
-import Navbar from "../components/layout/Navbar.jsx";
-import Footer from "../components/layout/footer.jsx";
-import AppRouter from "../routes/AppRouter.jsx";
+import AppRouter from "./routes/routes.jsx";
 
 function App() {
-  return (
-    <div className="app-shell">
-      <Navbar />
-      <main className="app-content">
-        <AppRouter />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
